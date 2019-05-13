3a421d05b0a51d9a2a430acd8ba461ddadf75ea8
Nathan95 <nathan@mediazoo.tv>
2019-05-13 16:22:42 +0100

    added google map integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import data from './data';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+          <Map google={this.props.google} zoom={4}>
+            {Object.keys(data).map((item, i) => (
+              <Marker key={i} onClick={this.onMarkerClick}
+                name={data[item].name}
+                position={{lat: data[item].latitude, lng: data[item].longitude}}
+              />
+            ))}
+            
+            <InfoWindow
+               marker={this.state.activeMarker}
+               visible={this.state.showingInfoWindow}
+              >
+              <div>{this.state.selectedPlace.name}</div>
+           </InfoWindow>
+          </Map>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyB7OghXV0bIeYRiGvNUCLoWNZN2MKLLAoQ')
+})(App)
